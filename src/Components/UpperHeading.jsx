04aa42ae2d7961e4dbd442b24537bf1a24afa8d1
Somnath-Mondal-97 <75529615+Somnath-Mondal-97@users.noreply.github.com>
2024-02-23 10:09:28 +0530

    first commit

---
+++ src/Components/UpperHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './UpperHeading.css'
+
+const UpperHeading = () => {
+  return (
+    <div>
+      <h1 className='mt-3 upper-heading'>Welcome To Star Wars Universe</h1>
+    </div>
+  )
+}
+
+export default UpperHeading
