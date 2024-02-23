e0a1b539e9ffe50ed195d6d218de260c3b958792
Somnath-Mondal-97 <75529615+Somnath-Mondal-97@users.noreply.github.com>
2024-02-23 10:25:40 +0530

    Deployed Website

--- src/Components/UpperHeading.jsx
+++ src/Components/UpperHeading.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './UpperHeading.css'
+import { Link } from 'react-router-dom'
 
 const UpperHeading = () => {
   return (
     <div>
-      <h1 className='mt-3 upper-heading'>Welcome To Star Wars Universe</h1>
+        <Link to="/"><h1 className='mt-3 upper-heading'>Welcome To Star Wars Universe</h1></Link>
+      
     </div>
   )
 }
